b6b694d6cf2d33861525ca37835c007569474b2e
Ruslan Abbasov <abbasov@sibext.com>
2022-01-28 11:18:53 +0700

    ListsTableViewController

---
+++ checkListMVVM/View/ListsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListsViewController.swift
+//  checkListMVVM
+//
+//  Created by abbasov on 28.01.2022.
+//
+
+import UIKit
+
+class ListsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+     }
+
+}
