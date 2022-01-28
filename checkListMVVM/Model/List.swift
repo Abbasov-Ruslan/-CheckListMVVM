eecf7bdec80faf4e5d7b0ee03757c39bb314ef97
Ruslan Abbasov <abbasov@sibext.com>
2022-01-28 11:37:36 +0700

    add List model

---
+++ checkListMVVM/Model/List.swift
@@ -0,0 +1,49 @@
+//
+//  List.swift
+//  checkListMVVM
+//
+//  Created by abbasov on 28.01.2022.
+//
+
+import Foundation
+
+protocol ListProtocol {
+    var name: String {get set}
+    var itemsArray: [AnyObject] {get set}
+
+    func addItem(item: AnyObject)
+    func deleteItem(id: Int)
+}
+
+class ItemsList: ListProtocol {
+    var name: String
+    
+    var itemsArray: [AnyObject] = []
+    
+    func addItem(item: AnyObject) {
+        itemsArray.append(item)
+    }
+    
+    func deleteItem(id: Int) {
+        itemsArray.remove(at: id)
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class ListOfLists: ListProtocol {
+    var name = "ListOfLists"
+    var itemsArray: [AnyObject] = []
+    
+    func addItem(item: AnyObject) {
+        itemsArray.append(item)
+    }
+    
+    func deleteItem(id: Int) {
+        itemsArray.remove(at: id)
+    }
+    
+    init(){}
+}
